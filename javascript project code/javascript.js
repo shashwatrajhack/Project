704f255940d17aefc4634be519750cce28609b45
shashwatraj <er.shashwatraj@gmail.com>
2022-01-19 19:31:48 +0530

    maths game complete code

--- javascript project code/javascript.js
+++ javascript project code/javascript.js
@@ -2,6 +2,7 @@ var playing = false;
 var score;
 var action;
 var timeremaining;
+var correctAnswer;
 
 
 
@@ -20,14 +21,47 @@ score;
                timeremaining = 60;
 
 document.getElementById("timeremainingvalue").innerHTML
- = timeremaining;      
+ = timeremaining;
+
+              hide("gameOver");      
 
 document.getElementById("startreset").innerHTML = 
 "Reset Game";
 
          startCountdown();
+
+         generateQA();
 	}
 }
+
+
+for(i=1; i<5; i++){
+	document.getElementById("box"+i).onclick = function(){
+		if(playing == true){
+			if(this.innerHTML == correctAnswer){
+				score++;
+
+	document.getElementById("scorevalue").innerHTML = score;
+	
+	       hide("wrong");
+	       show("correct");
+	       setTimeout(function(){
+	       	hide("correct");
+	       },1000);
+
+	       generateQA();			
+			}else{
+
+				hide("correct");
+				show("wrong");
+				setTimeout(function(){
+					hide("wrong");
+				},1000);
+			}
+		}
+	}
+}
+
 //if we are playing
 //if we are not playing
 //set score to 0
@@ -58,7 +92,7 @@ function startCountdown(){
 		if(timeremaining == 0){
 			stopCountdown();
 
-			show(gameOver);
+			show("gameOver");
 
 document.getElementById("gameOver").innerHTML = 
 "<p>Game Over !</p><p>Your score is " + score + ".</p>";
@@ -68,6 +102,8 @@ document.getElementById("gameOver").innerHTML =
      hide("wrong");
      playing = false;
 
+	document.getElementById("startreset").innerHTML = 
+	"Start Game";
 					}
 	}, 1000);
 }
@@ -84,4 +120,32 @@ function hide(Id){
 function show(Id){
 	document.getElementById(Id).style.display 
 	= "block";
+}
+//generate question and multipke answers
+
+function generateQA(){
+	var x = 1+ Math.round(9*Math.random());
+	var y = 1+ Math.round(9*Math.random());
+	correctAnswer = x*y;
+	document.getElementById("question").innerHTML = x + "x" + y;
+	var correctPosition = 1+ Math.round(3*Math.random());
+	document.getElementById("box"+correctPosition).innerHTML = 
+	correctAnswer;
+
+	var answers = [correctAnswer];
+
+	for(i=1; i<5; i++){
+		if(i !=correctPosition){
+			var wrongAnswer;
+			do{
+				wrongAnswer = (1+ Math.round(9*Math.random()))
+				*(1+ Math.round(9*Math.random()));
+			}while(answers.indexOf(wrongAnswer)>-1)
+
+			document.getElementById("box"+i).innerHTML = 
+			wrongAnswer;
+
+			answers.push(wrongAnswer);
+		}
+	}
 }
